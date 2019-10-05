f3d18720f34f341a2d64fe2be9ef8c267ecf55da
sobkowiak660 <sara.sobkowiak18@gmail.com>
2019-10-05 11:30:19 +0200

    gv

---
+++ Podstawy Programowania/1_first_project/1_first_project/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace _1_first_project
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Imię:");
+			Console.WriteLine("Janusz");
+			/*komentarz
+			 * w
+			 * kilku
+			 * liniach
+			 */
+
+			/*	#
+			 *	##
+			 *	###
+			 */
+			Console.WriteLine("#");
+				Console.WriteLine("##");
+					Console.WriteLine("###");
+
+			Console.WriteLine();
+
+			Console.WriteLine("#\n##\n###");
+			Console.WriteLine();
+			string name;
+			name = "Anna";
+			
+   Console.WriteLine(name);
+			//+ konkatenacja
+			Console.WriteLine("Masz na imię: " + name);
+			Console.WriteLine("Masz na imię: {0}",name);
+			Console.WriteLine();
+
+			int age = 20;
+			Console.WriteLine("Masz na imię: {0}, wiek: {1}",name,age );
+
+			sbyte age1 = 20;
+			Console.WriteLine("Masz na imię: {0}, wiek: {1}", name, age1);
+			Console.ReadKey();
+		}
+	}
+}
