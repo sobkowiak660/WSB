45268f16df7cf5cc296d1e45979d8f76404df2f8
sobkowiak660 <56188080+sobkowiak660@users.noreply.github.com>
2019-11-24 11:21:20 +0100

    Add files via upload

---
+++ referencje.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace zadanko_znowu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            Console.WriteLine("Wartość zmiennej x przed wywołaniem funkcji Increment: {0}",x);
+            Increment(ref x);
+            Increment(ref x);
+            Increment(ref x);
+            Increment(ref x);
+            Console.ReadKey();
+        }
+        static void Increment(ref int x)
+        {
+            x += 10;
+            Console.WriteLine("Zmienna x wewnątrz funkcji wynosi: {0}",x);
+        }
+    }
+}
---
+++ suma cyfr w liczbie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suma_cyfr_w_liczbie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine(Add(1234));
+            Console.ReadKey();
+            
+        }
+        static int Add(int x)
+        {
+            int result = 0;
+
+            do
+            {
+                result += x % 10;
+                x /= 10;
+            } while (x != 0);
+            return result;
+        }
+
+       
+    }   
+}
+
